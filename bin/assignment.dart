e273f723f3e668d8b1e6635d120ad513ad125d45
abdul_kader <kdrbipul@gmail.com>
2023-12-09 22:59:10 +0600

    new line add

--- bin/assignment.dart
+++ bin/assignment.dart
@@ -1,5 +1,45 @@
-import 'package:assignment/assignment.dart' as assignment;
+// create abstract class name of "Role" start here
+abstract class Role{
+  // create a "displayRole()" named method class name of Role
+  void displayRole();
+}
+// "Role" class end here
 
-void main(List<String> arguments) {
-  print('Hello world: ${assignment.calculate()}!');
+
+// create a class name of "Person" and implement of "Role" class start here
+class Person implements Role{
+  String name;
+  int age;
+  String address;
+
+  Person(this.name, this.age, this.address);
+}
+// "Person" class end here
+
+
+//create a class name of "Student" start here
+class Student extends Person{
+  int studentID = 78986;
+  String grade = 'A+';
+  list <int> courseScores = [45,56,79,98];
+
+  Student(this.studentID, this.grade, this.list);
+
+
+  @override
+  void displayRole(){
+    print("Role: Student");
+  }
+
+  /*int avarage = 0;
+  int sum = 0;
+  for(int i = 0, i<courseScores.length; i++){
+    sum += courseScores;
+  }*/
+}
+
+
+
+void main(){
+
 }
