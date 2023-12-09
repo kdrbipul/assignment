b3261370c3bfd27ce5b4c90e7f4cbaf677b6acb9
abdul_kader <kdrbipul@gmail.com>
2023-12-10 00:53:55 +0600

    new line add

--- bin/assignment.dart
+++ bin/assignment.dart
@@ -1,45 +1,76 @@
-// create abstract class name of "Role" start here
-abstract class Role{
-  // create a "displayRole()" named method class name of Role
+// Define the Role interface
+abstract class Role {
   void displayRole();
 }
-// "Role" class end here
 
-
-// create a class name of "Person" and implement of "Role" class start here
-class Person implements Role{
+// Implement the Person class
+class Person implements Role {
   String name;
   int age;
   String address;
 
+  // create Constructor class name of "Person"
   Person(this.name, this.age, this.address);
+
+  // Getter methods
+  String get getName => name;
+  int get getAge => age;
+  String get getAddress => address;
+
+  // Implement the displayRole method from the Role interface
+  @override
+  void displayRole() {
+    print("I am a generic person.");
+  }
 }
-// "Person" class end here
 
 
-//create a class name of "Student" start here
+// create a class name of "Student" start here
 class Student extends Person{
-  int studentID = 78986;
+
+  // create a attributes class name of "Student"
+  int studentID = 34556;
   String grade = 'A+';
-  list <int> courseScores = [45,56,79,98];
+  List <int> courseScores = [75,23,55,65];
 
-  Student(this.studentID, this.grade, this.list);
-
+//   create a constructor class name of Student
+Student(this.studentID, this.grade, String name, int age,
+    String address) : super(name, age,address);
 
   @override
-  void displayRole(){
+  void displayRole() {
     print("Role: Student");
   }
 
-  /*int avarage = 0;
-  int sum = 0;
-  for(int i = 0, i<courseScores.length; i++){
-    sum += courseScores;
+  /*void avg(){
+    int average = 0;
+    double sum = 0;
+    // int i = 0;
+
+    if( courseScores[i] = 0; i <courseScores.length; i++){
+      sum += courseScores[i];
+    }
+    sum = sum/courseScores.length;
+    print(sum);
   }*/
 }
 
+void main() {
+  // Example usage
+  /*Person personObj = Person("John Doe", 25, "123 Main St");
+
+  print("Name: ${personObj.getName}");
+  print("Age: ${personObj.getAge}");
+  print("Address: ${personObj.getAddress}");
+
+  personObj.displayRole(); // Output: I am a generic person.f*/
 
 
-void main(){
-
+  // create a object class name of Student
+  Student studentObj = Student(34556, "A+", "Alka Yagnik", 55, "New Delhi");
+  studentObj.displayRole();
+  print("Name: ${studentObj.getName}");
+  print("Age: ${studentObj.getAge}");
+  print("Address: ${studentObj.getAddress}");
+  // studentObj.avg();
 }
